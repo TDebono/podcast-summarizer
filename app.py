d4c6b14d93b4a8eb8af8e70d864d5c86c956941a
Timo Debono <timo.debono@gmail.com>
2022-03-22 20:09:27 +0100

    adjust title

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ st.set_page_config(
     page_title="Podscript - Probably the easiest way to transcribe your podcast", page_icon="✍🏼", initial_sidebar_state="expanded"
 )
 
+st.title("Podscript - Probably the easiest way to transcribe your podcast ✍🏼")
 uploaded_file = st.file_uploader("Upload your podcast audio file")
 
 st.button('Transcribe!')
