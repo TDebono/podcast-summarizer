45e95a7c90f9ff8e781bda38505984aae964f978
Timo Debono <timo.debono@gmail.com>
2022-04-09 23:44:38 +0200

    start building streamlit

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ st.set_page_config(
     page_title="Podscript - Probably the easiest way to transcribe your podcast", page_icon="✍🏼", initial_sidebar_state="expanded"
 )
 
-st.title("Podscript - Probably the easiest way to transcribe your podcast ✍🏼")
+# st.title("Podscript - Probably the easiest way to transcribe your podcast ✍🏼")
+st.title("blank")
 uploaded_file = st.file_uploader("Upload your podcast audio file")
 
 st.button('Transcribe!')
