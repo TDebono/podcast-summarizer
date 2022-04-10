031ff46ed8dff59ed33a5f758730a60a78bf84cb
Timo Debono <timo.debono@gmail.com>
2022-04-10 21:21:56 +0200

    build streamlit

--- app.py
+++ app.py
@@ -60,11 +60,17 @@ if go:
             polling_response = requests.get(transcript_endpoint+"/"+transcript_response.json()['id'], headers=headers)
             print("File is", polling_response.json()['status'])
         
-    final_transcription = polling_response.json()['text']
-    st.write(final_transcription)
+        final_transcription = polling_response.json()['text']
 
-    st.download_button(
-        label="Download transcript",
-        data=final_transcription,
-        file_name='transcript.txt'
-    )
+        st.header("Your transcript is ready!")
+        st.subheader("Go ahead and copy the transcript below, or simply download it directly as a text file.\
+                    No piece of software is perfect and we are continuously improving our algorithms, so you might want to briefly review the transcript after having downloaded it.\
+                    Thanks for your trust and support! 🚀")
+
+        st.write(final_transcription)
+
+        st.download_button(
+            label="Download transcript",
+            data=final_transcription,
+            file_name='transcript.txt'
+        )
